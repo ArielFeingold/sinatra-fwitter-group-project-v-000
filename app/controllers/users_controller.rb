11f7d41fdb3de98eb310e36f09328ad1ace78b9b
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-06 14:59:45 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,7 @@ class UsersController < ApplicationController
 
 
   post '/login' do
+    binding.pry
   @user = User.find_by(:username => params[:username])
     if @user != nil && @user.password == params[:password]
       session[:user_id] = @user.id
