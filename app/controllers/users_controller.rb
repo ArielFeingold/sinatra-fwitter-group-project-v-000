1b61c08592d69aad1be6aeeb76592e4dc6dd4422
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-06 15:10:04 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ binding.pry
   post '/login' do
     binding.pry
   @user = User.find_by(:username => params[:username])
-    if @user != nil && @user.password == params[:password]
+    if @user != nil && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect to '/tweets'
     else
